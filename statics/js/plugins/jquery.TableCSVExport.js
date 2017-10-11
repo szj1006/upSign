52094a30cd22af9ee4749c9301ad90a4f0c453fc
SangSir <szj1006@vip.qq.com>
2017-10-11 09:12:45 +0800

    Update

--- statics/js/plugins/jquery.TableCSVExport.js
+++ statics/js/plugins/jquery.TableCSVExport.js
@@ -11,7 +11,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 jQuery.fn.TableCSVExport = function (options) {
     var options = jQuery.extend({
-        separator: ',',
+        separator: "\t,",
         header: [],
         columns: [],
         extraHeader: "",
